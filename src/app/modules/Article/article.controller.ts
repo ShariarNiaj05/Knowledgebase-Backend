cfeaa7aac9b788230e29cc25c304f8aaf61957db
Shariar Islam <shariarn85@gmail.com>
2025-07-24 17:54:33 +0600

    API: create article service function updated

--- src/app/modules/Article/article.controller.ts
+++ src/app/modules/Article/article.controller.ts
@@ -4,17 +4,21 @@ import catchAsync from "../../../shared/catchAsync";
 import sendResponse from "../../../shared/sendResponse";
 import status from "http-status";
 import { ArticleService } from "./article.service";
+import { IAuthUser } from "../../interfaces/common";
 
-const createArticles = catchAsync(async (req: Request, res: Response) => {
-  const result = await ArticleService.createArticle(req.body);
+const createArticles = catchAsync(
+  async (req: Request & { user?: IAuthUser }, res: Response) => {
+    const user = req.user as IAuthUser;
+    const result = await ArticleService.createArticle(req.body);
 
-  sendResponse(res, {
-    statusCode: status.OK,
-    success: true,
-    message: "Articles Created Successfully!",
-    data: result,
-  });
-});
+    sendResponse(res, {
+      statusCode: status.OK,
+      success: true,
+      message: "Articles Created Successfully!",
+      data: result,
+    });
+  }
+);
 
 export const ArticleController = {
   createArticles,
--- src/app/modules/Article/article.service.ts
+++ src/app/modules/Article/article.service.ts
@@ -1,9 +1,18 @@
+import { IAuthUser } from "./../../interfaces/common";
 import status from "http-status";
 import ApiError from "../../errors/ApiError";
 import prisma from "../../../shared/prisma";
 
-const createArticle = async (req: any) => {
-  const { title, body, tags } = req.body;
+const createArticle = async (user: IAuthUser, payload: any) => {
+  const { title, body, tags } = payload;
+
+  // check if user exist
+
+  const userData = await prisma.user.findUniqueOrThrow({
+    where: {
+      email: user?.email,
+    },
+  });
 
   // Validate fields
   if (!title || !body || !Array.isArray(tags)) {
@@ -19,9 +28,11 @@ const createArticle = async (req: any) => {
       title,
       body,
       tags,
-      userId: req.user!.userId,
+      userId: userData.id,
     },
   });
+
+  return newArticle;
 };
 
 export const ArticleService = {
